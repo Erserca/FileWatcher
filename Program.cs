ee32920d2ff092a36927aa519b40ad358603233c
Erdoğan Serhat ÇALIŞKAN <e.serhat.caliskan@gmail.com>
2022-02-28 14:54:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net.NetworkInformation;
 using System.Net;
@@ -10,7 +10,6 @@ namespace Watcher
         static void Main(){
             
             using var watcher = new FileSystemWatcher(@"C:\");
-
             watcher.NotifyFilter = NotifyFilters.Attributes
                                  | NotifyFilters.CreationTime
                                  | NotifyFilters.DirectoryName
@@ -37,30 +36,32 @@ namespace Watcher
         
         private static void OnChanged(object sender, FileSystemEventArgs e)
         {
-            if (e.ChangeType != WatcherChangeTypes.Changed || e.FullPath == @"C:\Users\Erdo\source\repos\FileWatcher\hareket.txt")
+            string programYolu = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase );
+            
+            if (e.ChangeType != WatcherChangeTypes.Changed)
             {
                 return;
             }
             Console.WriteLine($"Değiştirilen: {e.FullPath}");
-            File.AppendAllText(@"hareket.txt","Değiştirilme, " + e.FullPath + ", " + DateTime.Now + "\n");
+           
         }
 
         private static void OnCreated(object sender, FileSystemEventArgs e)
         {
             string value = $"Oluşturulan: {e.FullPath}";
             Console.WriteLine(value);
-            File.AppendAllText(@"hareket.txt","Oluşturulma, " + e.FullPath + ", " + DateTime.Now + "\n");
+            
         }
 
         private static void OnDeleted(object sender, FileSystemEventArgs e){
             Console.WriteLine($"Silinen: {e.FullPath}");
-            File.AppendAllText(@"hareket.txt","Silinme, " + e.FullPath + ", " + DateTime.Now + "\n");
+            
         }
 
         private static void OnRenamed(object sender, RenamedEventArgs e)
         {
             Console.WriteLine($"Yeniden Adlandırma Eski: {e.OldFullPath} " + $" Yeni: {e.FullPath}");
-            File.AppendAllText(@"hareket.txt","İsim Değiştirme, Eski: " + e.OldFullPath + ", Yeni: " + e.FullPath+ ", "+ DateTime.Now + "\n");
+            
         }
 
         private static void OnError(object sender, ErrorEventArgs e) =>
